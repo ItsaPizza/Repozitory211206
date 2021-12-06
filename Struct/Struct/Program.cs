976775547652dac42828769dd58ac1831a71b1c5
Laurynas <laurynas.balsys91@gmail.com>
2021-12-06 19:54:05 +0200

    First Struct

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    internal class Program
+    {
+        public struct Metinis
+        {
+            public int Pirmas;
+            public int Antras;
+            public int Trecias;
+
+            public Metinis(int pirmas, int antras, int trecias)
+            {
+                Pirmas = pirmas;
+                Antras = antras;
+                Trecias = trecias;
+            }
+
+            public double IsvestiVidurki() => (Pirmas + Antras + Trecias) / 3;
+
+        }
+        
+        static void Main(string[] args)
+        {
+            Metinis metinis = new Metinis(10, 10, 9);
+            double vidurkis = metinis.IsvestiVidurki();
+            Console.WriteLine($"Metinis vidurkis: {vidurkis}");
+            Console.ReadKey();
+        }
+    }
+}
